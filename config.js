66f230a6e8b2fcc2500cacfd7b77b6e3ffae954c
Krzysztof Lipka <klpka@n7mobile.com>
2024-03-07 18:49:45 +0100

    DB config changed

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   db: {// TODO Ensure the safety of
-    host: "sql11.freemysqlhosting.net",
-    user: "sql11686645",
-    password: "XwZSZvxf6H",
-    database: "sql11686645",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "old_map",
   }
 };
 module.exports = config;
