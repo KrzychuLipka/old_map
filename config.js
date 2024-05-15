828eb550598381c90820808fd0d3856606652923
Krzysztof Lipka <klpka@n7mobile.com>
2024-05-15 11:30:49 +0200

    Old map project finished

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
     db: {// TODO Ensure the safety of
-        host: "sql11.freemysqlhosting.net",
-        user: "sql11705005",
-        password: "pLI65fG33M",
-        database: "sql11705005",
+        host: "localhost",
+        user: "root",
+        database: "old_map",
   }
 };
 module.exports = config;
